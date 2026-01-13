f7210e87ca698bb92bc50f5c4d53c59bb43d4ad1
Oleg Plema <olegplema@gmail.com>
2026-01-13 22:42:48 +0200

    chore(query): initialize url-query-service project structure

---
+++ url-query-service/src/main/java/com/plema/url_query_service/UrlQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.plema.url_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlQueryServiceApplication.class, args);
+	}
+
+}
